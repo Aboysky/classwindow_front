d631bb45b6c846ba0bf8c541aa66bf3e71151f61
Huan <huan.l@outlook.com>
2021-04-10 12:51:08 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+      这是我的第一个vue项目
   </div>
   <router-view/>
 </template>
